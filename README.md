60fcff1b06ead1f74f2572d6ab257a42e1edb7bd
jstewart <jstewart@elevateunited.com>
2024-08-20 10:21:46 -0600

    othello game

--- README.md
+++ README.md
@@ -1,2 +1,79 @@
-# othello
-Othello, also known as Reversi, is a game between two players, denoted by black and white.
+/**
+ * Othello, also known as Reversi, is a game between two players, denoted by black
+ * and white.
+ *
+ * Play happens on an 8x8 grid. Game pieces are discs with a black side and a
+ * white side. The face-up side of a piece indicates its current owner.
+ *
+ * The game begins with two black pieces and two white pieces, as shown:
+ *
+ *   a b c d e f g h
+ * 1
+ * 2
+ * 3
+ * 4      B W
+ * 5      W B
+ * 6
+ * 7
+ * 8
+ *
+ * Players alternate turns, beginning with black.
+ *
+ * A player's turn consists of placing a new piece of their color on an empty space
+ * and then flipping the opponent's pieces.
+ *
+ * A player flips lines of one or more opposing pieces when they are bookended
+ * (surrounded) by the newly placed piece and one of their existing pieces. The line
+ * including the bookends must be contiguous (no gaps). Lines of flipped pieces
+ * can be othogonal or diagonal. Multiple lines may be flipped in a single turn.
+ * (Note: One of the two surrounding pieces MUST be the newly placed piece.)
+ *
+ * For example, in the following game, black plays g6. This move flips the white
+ * pieces at c6, d6, e6, f5, and f6 to black.
+ *
+ *   a b c d e f g h       a b c d e f g h       a b c d e f g h
+ * 1                     1                     1
+ * 2                     2                     2
+ * 3       W B W         3       W B W         3       W B W
+ * 4     W B B W B       4     W B B W B       4     W B B W B
+ * 5   W B W B W         5   W B W B *         5   W B W B B
+ * 6   B W W W W         6   B * * * * B       6   B B B B B B
+ * 7                     7                     7
+ * 8                     8                     8
+ *
+ * Every move must flip at least one piece. If a player cannot move, their turn is
+ * skipped.
+ *
+ * For example, in the following game, white has no legal move:
+ *
+ *   a b c d e f g h
+ * 1       W W W   W
+ * 2     W W W W   W
+ * 3   W W W B W W W
+ * 4     W B B W B W
+ * 5 W W W W W W B W
+ * 6   W W W W W W W
+ * 7     W W W W W W
+ * 8 B B B B B B B W
+ *
+ * When neither player can move, the game ends.
+ *
+ * At the end of the game, the player with the most pieces wins. If players have
+ * the same number of pieces, the game is a tie.
+ *
+ * Write a program that two people can use to play a game of Othello.
+ *
+ * A fully working program should:
+ *   * validate attempted moves
+ *   * execute moves
+ *   * skip turns
+ *   * end the game
+ *   * display the winner
+ *
+ * If you have extra time, create a simple AI to play the game.
+ *
+ * Pace your development such that the program works as much as possible by the
+ * end of the alloted time; i.e. it should not be in a "broken" state.
+ *
+ * The beginnings of a program are provided. Feel free to modify the program as desired.
+ */
---
+++ othello.js
@@ -0,0 +1,229 @@
+const readline = require('node:readline');
+
+class Coordinate {
+  constructor(row, col) {
+    this.row = row;
+    this.col = col;
+  }
+
+  toString() {
+    return `Coordinate(row=${this.row}, col=${this.col})`;
+  }
+}
+
+class Color {
+  static BLACK = new Color('B');
+  static WHITE = new Color('W');
+
+  constructor(name) {
+    this.name = name;
+  }
+
+  abbreviation() {
+    return this.name;
+  }
+
+  toString() {
+    switch (this.name) {
+      case 'B':
+        return 'black';
+      case 'W':
+        return 'white';
+    }
+    throw new Error(`Unknown color: ${this.name}`);
+  }
+}
+
+class Board {
+  constructor(size) {
+    this.size = size;
+    this.positions = new Array(size).fill(null).map(() => new Array(size).fill(null));
+  }
+
+  toString() {
+    let result = '';
+    result += '  ';
+    for (let col = 0; col < this.size; col++) {
+      result += String.fromCharCode('a'.charCodeAt(0) + col);
+      result += ' ';
+    }
+    result += '\n';
+    for (let row = 0; row < this.size; row++) {
+      result += String.fromCharCode('1'.charCodeAt(0) + row);
+      result += ' ';
+      for (let col = 0; col < this.size; col++) {
+        const position = this.positions[row][col];
+        result += position !== null ? position.abbreviation() : ' ';
+        if (col < this.size - 1) {
+          result += ' ';
+        }
+      }
+      result += '\n';
+    }
+    return result;
+  }
+}
+
+class CoordinateParseException extends Error {}
+
+class Othello {
+  static SIZE = 8;
+
+  parseCoordinate(input) {
+    if (input.length !== 2) {
+      throw new CoordinateParseException("Input must be length 2");
+    }
+    const row = input.charCodeAt(1) - '1'.charCodeAt(0);
+    if (row < 0 || Othello.SIZE <= row) {
+      throw new CoordinateParseException("Row out of bounds");
+    }
+    const col = input.charCodeAt(0) - 'a'.charCodeAt(0);
+    if (col < 0 || Othello.SIZE <= col) {
+      throw new CoordinateParseException("Column out of bounds");
+    }
+    return new Coordinate(row, col);
+  }
+
+  async play() {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+      prompt: 'Othello > ',
+    });
+
+    const board = new Board(Othello.SIZE);
+    board.positions[3][3] = Color.WHITE;
+    board.positions[3][4] = Color.BLACK;
+    board.positions[4][3] = Color.BLACK;
+    board.positions[4][4] = Color.WHITE;
+
+    let turn = Color.BLACK;
+
+    while (true) {
+      console.log(board.toString());
+      try {
+        const input = await new Promise(res => rl.question(`Enter move for ${turn}: `, res));
+        const move = this.parseCoordinate(input);
+         
+        const piecesChanged = getChangedPieces(move, board, turn)
+        if(piecesChanged.length > 0){
+          piecesChanged.forEach(coord => board.positions[coord.row][coord.col] = turn);
+          board.positions[move.row][move.col] = turn
+        } else{
+          throw new CoordinateParseException("No pieces would be turned or coordinate already taken.")
+        }
+
+        turn = opponentsColor(turn)
+
+        if(skipTurn(board, turn)){
+          turn = opponentsColor(turn)
+          if(skipTurn(board, turn)){
+            console.log("Game is over no more possible moves.")
+            endGame(board)
+            break
+          }
+        }
+      } catch (e) {
+        console.log("ERROR: ", e)
+        if (e instanceof CoordinateParseException) {
+          console.log(`Invalid move: ${e.message}`);
+          console.log();
+          continue;
+        } else {
+          throw e;
+        }
+      }
+      
+    }
+  }
+}
+
+function opponentsColor(turn){
+  return turn == Color.BLACK ? Color.WHITE : Color.BLACK
+}
+
+function skipTurn(board, turn){
+  for (let row = 0; row < board.size; row++) {
+    for (let col = 0; col < board.size; col++) {
+      const move = new Coordinate(row, col)
+      if(board.positions[move.row][move.col] == null){
+        const piecesChanged = getChangedPieces(move, board, turn);
+        if(piecesChanged.length > 0){
+          return false
+        }
+      }
+
+    }
+  }
+  return true
+}
+
+function getChangedPieces(move, board, turn){
+  var piecesChanged = []
+  if (board.positions[move.row][move.col] != null){
+    return piecesChanged;
+  } 
+  const opponentColor = opponentsColor(turn)
+  options.forEach(option => {
+    const changesArr = trackPiecesChanged(board, { ...move }, option, opponentColor, []);
+    piecesChanged = [...piecesChanged, ...changesArr];
+  });
+
+  return piecesChanged;
+}
+
+function trackPiecesChanged(board, coordinate, option, opponentColor, coordArr){
+  coordinate.row = coordinate.row+option[0]
+  coordinate.col = coordinate.col+option[1]
+  if(coordinate.row > board.size-1 || coordinate.row < 0 || coordinate.col > board.size-1 || coordinate.col < 0){
+    return []
+  }
+
+  if (board.positions[coordinate.row][coordinate.col] !== opponentColor) {
+    if (board.positions[coordinate.row][coordinate.col] === null) {
+      return [];
+    }
+    return coordArr.length > 0 ? coordArr : [];
+  }
+  coordArr.push({ ...coordinate })
+  return trackPiecesChanged(board, coordinate, option, opponentColor, coordArr);
+}
+
+function endGame(board){
+  console.log(board.toString())
+  var blackScore = 0
+  var whiteScore = 0
+  var winner = "ITS A TIE!"
+
+  board.positions.forEach(row => {
+    row.forEach(position => {
+      if (position === Color.BLACK) blackScore++;
+      else if (position === Color.WHITE) whiteScore++;
+    });
+  });
+
+  if(blackScore > whiteScore){
+    winner = "The winner is " + Color.BLACK
+  } else if(whiteScore > blackScore){
+    winner = "The winner is " + Color.WHITE
+  }
+
+  console.log("Black score: ", blackScore);
+  console.log("White score: ", whiteScore)
+  console.log(winner)
+}
+
+//these are the options for checking if moves are valid and which pieces are turned
+const options = [
+  [0,1],
+  [0,-1],
+  [1,0],
+  [-1,0],
+  [1,1],
+  [-1,-1],
+  [-1,1],
+  [1,-1]
+]
+
+const othello = new Othello();
+othello.play();
